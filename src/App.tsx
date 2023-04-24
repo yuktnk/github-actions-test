6902091ba07c146291e2115346e4dda368146aa3
yuktnk <yuki0wl@gmail.com>
2023-04-25 08:59:42 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React+
+          Learn React--04250900
         </a>
       </header>
     </div>
